a66ecc1d13bf97bc28d606073743159f01e2ed49
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2018-04-18 12:02:37 +0200

    Add integration test for HTTPHandle

---
+++ src/test/java/io/scif/locations/HTTPHandleIntegrationTest.java
@@ -0,0 +1,30 @@
+
+package io.scif.locations;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.scijava.Context;
+import org.scijava.io.http.HTTPLocation;
+
+import io.scif.img.ImgIOException;
+import io.scif.img.ImgOpener;
+
+/**
+ * tests opening larger images
+ * 
+ * @author gabriel
+ */
+public class HTTPHandleIntegrationTest {
+	
+	private Context ctx = new Context();
+
+	
+	@Test
+	public void testJPEG() throws MalformedURLException, URISyntaxException, ImgIOException {
+		ImgOpener opener = new ImgOpener(ctx);
+		opener.openImgs(new HTTPLocation("https://imagej.net/images/Diatoms.jpg"));
+	}
+}
